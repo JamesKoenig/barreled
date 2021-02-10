a808fda202a08699f28743b3c6dab9d0f5a00e22
James Koenig <Koenig.James@Live.com>
2021-02-10 14:47:54 -0800

    working through rails-redux actions for user auth

---
+++ frontend/actions/session.js
@@ -0,0 +1,40 @@
+import {
+  postUser,
+  deleteUser,
+  postSession,
+  deleteSession } as APIUtils from '../utils/session';
+
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
+export const LOGOUT_CURRENT_USER  = "LOGOUT_CURRENT_USER"
+export const REMOVE_USER          = "REMOVE_USER"
+
+const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+const removeUser = userId => ({
+  type: REMOVE_USER,
+  userId
+})
+
+export const createNewUser = formUser => dispatch =>
+  APIUtils.postUser(formUser)
+    .then( user => dispatch(receiveCurrentUser(user)) );
+
+export const deleteUser = userId => dispatch =>
+  APIUtils.deleteUser(userId)
+    .then( () => dispatch(removeUser(userId) );
+
+export const login = formUser => dispatch =>
+  APIUtils.postSession()
+    .then( user => dispatch(receiveCurrentUser(user)) );
+
+export const logout = () => dispatch =>
+  APIUtils.deleteSession()
+    .then( () => dispatch(logoutCurrentUser()) );
