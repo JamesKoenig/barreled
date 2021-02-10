63190b4e82449a9e4af6387b5b7528f99664e06a
James Koenig <Koenig.James@Live.com>
2021-02-10 15:54:18 -0800

    commented out remove user stuff so I can reference it later

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -1,35 +1,30 @@
-import {
-  postUser,
-  deleteUser,
-  postSession,
-  deleteSession } as APIUtils from '../utils/session';
-
+import * as APIUtils from '../utils/session';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
 export const LOGOUT_CURRENT_USER  = "LOGOUT_CURRENT_USER"
-export const REMOVE_USER          = "REMOVE_USER"
+// export const REMOVE_USER          = "REMOVE_USER"
 
-const receiveCurrentUser = user => ({
+const receiveCurrentUser = currentUserId => ({
   type: RECEIVE_CURRENT_USER,
-  user
+  currentUserId
 });
 
 const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
 });
 
-const removeUser = userId => ({
-  type: REMOVE_USER,
-  userId
-})
+// const removeUser = userId => ({
+//   type: REMOVE_USER,
+//   userId
+// })
 
 export const createNewUser = formUser => dispatch =>
   APIUtils.postUser(formUser)
     .then( user => dispatch(receiveCurrentUser(user)) );
 
-export const deleteUser = userId => dispatch =>
-  APIUtils.deleteUser(userId)
-    .then( () => dispatch(removeUser(userId) );
+// export const deleteUser = userId => dispatch =>
+//   APIUtils.deleteUser(userId)
+//     .then( () => dispatch(removeUser(userId)) );
 
 export const login = formUser => dispatch =>
   APIUtils.postSession()
