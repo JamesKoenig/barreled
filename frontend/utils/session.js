6750758b118f5fcdb7746155f3415d63ae94386d
James Koenig <Koenig.James@Live.com>
2021-02-10 14:02:55 -0800

    setting up frontend user/session util calls

---
+++ frontend/utils/session.js
@@ -0,0 +1,25 @@
+export const postUser = user => 
+  $.ajax({
+    url:    "api/users",
+    method: "POST",
+    data: { user }
+  });
+
+export const deleteUser = userId => 
+  $.ajax({
+    url:    `api/users/${userId}`,
+    method: "DELETE",
+  });
+
+export const postSession = user =>
+  $.ajax({
+    url:    "api/session",
+    method: "POST",
+    data: { user }
+  })
+
+export const deleteSession = () =>
+  $.ajax({
+    url:    "api/session",
+    method: "DELETE"
+  });
