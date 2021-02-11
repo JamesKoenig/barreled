fc1dba776ba1c4575394148cb9298164b9360f2d
James Koenig <Koenig.James@Live.com>
2021-02-10 16:27:21 -0800

    added configureStore function in store folder to refactor more code out
    of the barreled.jsx entry point, as well as to incorporate the root
    reducer into the codebase

--- frontend/barreled.jsx
+++ frontend/barreled.jsx
@@ -1,13 +1,12 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
-
+import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
 
-  const store = createStore(() => {}, {});
   ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/store/store.js
@@ -0,0 +1,8 @@
+import { 
+  createStore,
+  applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import RootReducer from '../reducers/root';
+
+export default (preloadedState = {}) =>
+  createStore(RootReducer, preloadedState, applyMiddleware(thunk));
