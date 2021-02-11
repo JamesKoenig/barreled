a7687177684653d710195c14828d8ebf737a457c
James Koenig <Koenig.James@Live.com>
2021-02-10 16:07:47 -0800

    removed superflous code from session actions regarding deleting a user
    I'll factor it in later as a much later feature.  it's not needed
    currently and complicates what would otherwise be a straightforward
    set of react-redux logic to deploy

--- frontend/actions/session.js
+++ frontend/actions/session.js
@@ -2,7 +2,6 @@ import * as APIUtils from '../utils/session';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
 export const LOGOUT_CURRENT_USER  = "LOGOUT_CURRENT_USER"
-// export const REMOVE_USER          = "REMOVE_USER"
 
 const receiveCurrentUser = currentUserId => ({
   type: RECEIVE_CURRENT_USER,
@@ -13,18 +12,9 @@ const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
 });
 
-// const removeUser = userId => ({
-//   type: REMOVE_USER,
-//   userId
-// })
-
 export const createNewUser = formUser => dispatch =>
   APIUtils.postUser(formUser)
     .then( user => dispatch(receiveCurrentUser(user)) );
-
-// export const deleteUser = userId => dispatch =>
-//   APIUtils.deleteUser(userId)
-//     .then( () => dispatch(removeUser(userId)) );
 
 export const login = formUser => dispatch =>
   APIUtils.postSession()
