9159f691ccbbfdf9c166dfc6f92680e1fedfd9e1
James Koenig <Koenig.James@Live.com>
2021-02-15 15:51:50 -0800

    changed root to wildcard in app.jsx's path

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,7 +4,7 @@ import   Feed             from './feed/feed_container';
 import { AuthSplitRoute } from '../utils/route_utils';
 
 export default () => (
-  <AuthSplitRoute path="/"
+  <AuthSplitRoute path="*"
                   left={Feed}
                   right={Splash} />
 )
