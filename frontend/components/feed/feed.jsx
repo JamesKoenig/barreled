4609ac4662738537f72309e60dc51d5d1003f541
James Koenig <Koenig.James@Live.com>
2021-02-15 15:49:36 -0800

    feed redirects everything to / for now

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import   React      from 'react';
+import { Switch,
+         Route,
+         Redirect } from 'react-router-dom';
 
 export default ({ logout }) => (
-  <>
-    <p>feed placeholder!</p>
-    <button onClick={() => logout()}>logout</button>
-  </>
+  <Switch>
+    <Route exact path="/">
+      <p>feed placeholder!</p>
+      <button onClick={() => logout()}>logout</button>
+    </Route>
+    <Route path="*">
+      <Redirect to="/" />
+    </Route>
+  </Switch>
 )
