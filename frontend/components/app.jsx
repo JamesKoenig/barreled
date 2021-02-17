14b66d58e75270a485599d7f8ca79337323cb5c9
James Koenig <Koenig.James@Live.com>
2021-02-16 16:14:36 -0800

    post display placeholder!

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,8 +2,15 @@ import   React            from 'react';
 import   Splash           from './splash/splash_container';
 import   Feed             from './feed/feed_container';
 import { AuthSplitRoute } from '../utils/route_utils';
+import { Switch,
+         Route          } from 'react-router'
+import PostShow from './posts/show';
 
 export default () => (
-  <AuthSplitRoute left={Feed}
-                  right={Splash} />
+  <Switch>
+    <Route exact path="/posts/:postId"
+                 component={PostShow} />
+    <AuthSplitRoute left={Feed}
+                    right={Splash} />
+  </Switch>
 )
