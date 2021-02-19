0d92001e33298170b3c17c7950fe538bd4cc4955
James Koenig <Koenig.James@Live.com>
2021-02-18 22:34:41 -0800

    removed header from feed

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -3,14 +3,11 @@ import { Switch,
          Route,
          Redirect } from 'react-router-dom';
 
-import Header from '../header/header';
-
 export default ({ logout }) => (
   <Switch>
     <Route exact path="/">
       <div id="feed">
         <button onClick={() => logout()}>logout</button>
-        <Header />
       </div>
     </Route>
     <Route path="*">
