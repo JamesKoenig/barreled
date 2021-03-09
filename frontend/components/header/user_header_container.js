f39b8bbc965e5f5949ad41f7d601515c0eb7c10e
James Koenig <Koenig.James@Live.com>
2021-03-08 22:03:36 -0800

    add logouot to the user header

--- frontend/components/header/user_header_container.js
+++ frontend/components/header/user_header_container.js
@@ -1,10 +1,14 @@
 import { withRouter } from 'react-router-dom';
 import { connect    } from 'react-redux';
 import { openModal  } from '../../actions/modal';
+import { logout     } from '../../actions/session';
 import   Header       from './base_header';
 
 const mDTP = dispatch => ({
-  navButtons: [["New Post",() => dispatch(openModal('newPost'))]],
+  navButtons: [
+    ["New Post",() => dispatch(openModal('newPost'))],
+    ["Logout", () => dispatch(logout())],
+  ],
 });
 
 export default withRouter(connect(null,mDTP)(Header));
