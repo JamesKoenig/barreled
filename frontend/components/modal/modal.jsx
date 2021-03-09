e5ba90595c3c10b9ed589e44e3f8bb5b61bce584
James Koenig <Koenig.James@Live.com>
2021-03-09 14:02:17 -0800

    modal opens and closes on click

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 class Modal extends React.Component {
   constructor(props) {
     super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.closeModal();
   }
 
   render() {
+    if(!this.props.modal) return null;
     return (
-      <div id="modal-background">
-        <div className="modal">
+      <div id="modal-background"
+           onClick={this.onClick} >
+        <div className="modal"
+             onClick={event => event.stopPropagation()}>
           <p>hello modal!</p>
         </div>
       </div>
--- frontend/components/modal/modal_container.jsx
+++ frontend/components/modal/modal_container.jsx
@@ -2,10 +2,12 @@ import   Modal        from './modal';
 import { closeModal } from '../../actions/modal';
 import { connect    } from 'react-redux';
 
-const mSTP = state => ({
+const mSTP = ({ui: { modal } })=> ({
+  modal,
 });
 
 const mDTP = dispatch => ({
+  closeModal: () =>  dispatch(closeModal()),
 });
 
 export default connect(mSTP,mDTP)(Modal);
