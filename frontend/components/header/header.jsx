b76b8f99a6365886aee30aa5bfa98e7c469939ef
James Koenig <Koenig.James@Live.com>
2021-03-09 17:25:18 -0800

    import from react-router-dom and not react-router

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthSplitRoute } from '../../utils/route_utils';
-import { useRouteMatch  } from 'react-router'
+import { useRouteMatch  } from 'react-router-dom'
 
 import   AuthHeader  from './auth_header_container';
 import   UserHeader  from './user_header_container';
