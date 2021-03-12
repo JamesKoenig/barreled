4aef86309b3fe233926d268f0838f21453c037c1
James Koenig <Koenig.James@Live.com>
2021-03-11 23:00:11 -0800

    add all paths to the Header
    the header needs to be able to use withRouter to parse out information
    about its location in a few different circumstances, this will make that
    step easier

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,17 +5,23 @@ import { AuthSplitRoute } from '../utils/route_utils';
 import { Switch,
          Route          } from 'react-router'
 import PostShow from './posts/show';
-import * as postUrls from '../utils/paths/posts';
+import * as postUrls    from '../utils/paths/posts';
+import * as sessionUrls from '../utils/paths/session';
 import Header from './header/header';
+
+const postUrlsStrings    = Object.values(postUrls);
+const sessionUrlsStrings = Object.values(sessionUrls);
 
 export default () => (
   <>
     <Switch>
-      <Route exact path={Object.values(postUrls)}
+      <Route exact path={postUrlsStrings}
                    component={PostShow} />
       <AuthSplitRoute left={Feed}
                       right={Splash} />
     </Switch>
-    <Header />
+    <Route path={[...postUrlsStrings,...sessionUrlsStrings,"*"]}>
+      <Header />
+    </Route>
   </>
 )
