faf86e8609713e5453bdb8043787ff7effd588e1
James Koenig <Koenig.James@Live.com>
2021-06-08 01:09:15 -0700

    add missing post CRUD operations to git

--- frontend/utils/posts.js
+++ frontend/utils/posts.js
@@ -1,9 +1,25 @@
 export const fetchPost = postId =>
   $.ajax({
-    url: `api/posts/${postId}`
+    url: `api/posts/${postId}`,
   })
 
-export const fetchFeed = () =>
+export const postPost = post =>
   $.ajax({
-    url: 'api/posts/feed'
+    url: '/api/posts/',
+    method: 'POST',
+    data: { post },
+  });
+
+export const updatePost = post =>
+  $.ajax({
+    url: `api/posts/${post.id}`,
+    method: 'PATCH',
+    data: { post },
   })
+
+export const deletePost = postId =>
+  $.ajax({
+    url: `api/posts/${postId}`,
+    method: 'DELETE',
+  })
+
