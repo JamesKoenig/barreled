45bded40c9960a8b25f7de5cb6c3b6894ebb285c
James Koenig <Koenig.James@Live.com>
2021-06-29 00:00:51 -0700

    reorganizing styling, fixing button placement

--- frontend/components/posts/post_box.jsx
+++ frontend/components/posts/post_box.jsx
@@ -4,13 +4,13 @@ export default ({ post, author }) => (
   <div className="post-display">
     <header className="post-header">
       <h3 className="author-name">{author.username}</h3>
-      <button onClick={ () => alert("no") }>hello world</button>
     </header>
     <div className="post-content">
       <p className="post-text">{post.body}</p>
     </div>
     <footer className="post-properties">
       <p>{`${post.totalLikes} likes`}</p>
+      <button onClick={ () => alert("no") }>hello world</button>
     </footer>
   </div>
 )
