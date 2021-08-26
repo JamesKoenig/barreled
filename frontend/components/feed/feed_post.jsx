86d11a79187e2d43b66446b9d40d8d43287c323b
James Koenig <Koenig.James@Live.com>
2021-08-26 13:53:32 -0700

    go too the post display when a feed's post is clicked

--- frontend/components/feed/feed_post.jsx
+++ frontend/components/feed/feed_post.jsx
@@ -1,9 +1,21 @@
 import React, { useEffect }  from 'react';
+import { useHistory } from 'react-router-dom';
 import PostBox from '../posts/post_box';
 
-export default ({post,author}) => {
+export default ({post,author,postId}) => {
+  const history = useHistory();
+  /* enclose the history object in the callback,
+   * as per the example in https://reactrouter.com/native/api/Hooks/usehistory
+   *  (retrieved August 26th 2021)
+   */
+  const onClick = () =>
+    history.push(`/posts/${postId}`);
+
   if(!post || !author) return null;
+  // add a div, so that onClick is called
   return (
-    <PostBox post={post} author={author} />
+    <div onClick={onClick}>
+      <PostBox post={post} author={author} />
+    </div>
   );
 }
