e09f978190c22b25cccfee96e708b66c6179e73d
James Koenig <Koenig.James@Live.com>
2021-08-26 19:03:57 -0700

    have the post display interact with user inputs

--- frontend/components/feed/feed_post.jsx
+++ frontend/components/feed/feed_post.jsx
@@ -3,19 +3,8 @@ import { useHistory } from 'react-router-dom';
 import PostBox from '../posts/post_box';
 
 export default ({post,author,postId}) => {
-  const history = useHistory();
-  /* enclose the history object in the callback,
-   * as per the example in https://reactrouter.com/native/api/Hooks/usehistory
-   *  (retrieved August 26th 2021)
-   */
-  const onClick = () =>
-    history.push(`/posts/${postId}`);
-
   if(!post || !author) return null;
-  // add a div, so that onClick is called
   return (
-    <div onClick={onClick}>
-      <PostBox post={post} author={author} />
-    </div>
+    <PostBox post={post} author={author} />
   );
 }
--- frontend/components/posts/post_box.jsx
+++ frontend/components/posts/post_box.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
+import { useHistory,useLocation } from 'react-router-dom';
 
-export default ({ post, author }) => (
-  <div className="post-display">
-    <header className="post-header">
-      <h3 className="author-name">{author.username}</h3>
-    </header>
-    <div className="post-content">
-      <p className="post-text">{post.body}</p>
+export default ({ post, author }) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  /* enclose the history object in the callback,
+   * as per the example in https://reactrouter.com/native/api/Hooks/usehistory
+   *  (retrieved August 26th 2021)
+   */
+  const boxOnClick = () => {
+    /* I can probably use some react router elements for this but this just
+     * gets it done. avert thine eyes lest it offends thee. */
+    if( !/\/posts\/.+/.test(location.pathname) ) {
+      history.push(`/posts/${post.id}`);
+    }
+  }
+
+  const etcOnClick = event => {
+    event.stopPropagation();
+    alert("etc action");
+  }
+
+  const likerOnClick = event => {
+    event.stopPropagation();
+    alert("button pressed");
+  }
+
+  return (
+    <div className="post-display"
+         onClick={boxOnClick} >
+      <header className="post-header">
+        <h3 className="author-name">{author.username}</h3>
+        <button onClick={etcOnClick}>...</button>
+      </header>
+      <div className="post-content">
+        <p className="post-text">{post.body}</p>
+      </div>
+      <footer className="post-properties">
+        <p>{`${post.totalLikes} likes`}</p>
+        <button onClick={ likerOnClick }>hello world</button>
+      </footer>
     </div>
-    <footer className="post-properties">
-      <p>{`${post.totalLikes} likes`}</p>
-      <button onClick={ () => alert("no") }>hello world</button>
-    </footer>
-  </div>
-)
+  )
+}
