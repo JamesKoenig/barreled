04c81362297fac268b4d3d7d453fc40f60bce6d9
James Koenig <Koenig.James@Live.com>
2021-08-30 14:08:16 -0700

    fix errors resulting from data types, misc cleanup
    remove excess whitespace, add missing semicolons, add like_error
    dispatch for later (more formal) error handling

--- frontend/actions/like_actions.js
+++ frontend/actions/like_actions.js
@@ -5,27 +5,45 @@ import {
 
 export const RECEIVE_LIKE = 'RECEIVE_LIKE';
 export const REMOVE_LIKE  = 'REMOVE_LIKE';
+export const LIKE_ERROR   = 'LIKE_ERROR';
 
 const receiveLike = like => ({
   type: RECEIVE_LIKE,
   like,
-})
+});
 
 const removeLike = postId => ({
   type: REMOVE_LIKE,
-  like, 
-})
+  like,
+});
+
+const likeError = err => ({
+  type: LIKE_ERROR,
+  err,
+});
+
+const handleLikeError = err => dispatch => {
+  //this is for debugging right now
+  // in lieu of proper error handling in ui
+  console.log(err.responseJSON);
+  dispatch(likeError(err));
+}
 
 export const like = postId => dispatch =>
   likePost(postId)
-    .then( like =>
-      dispatch(receiveLike(like)))
-    .catch( error =>
-      console.log(error));
+    .then(
+      like =>
+        dispatch(receiveLike(like)),
+      error =>
+        dispatch(handleLikeError(error))
+      )
 
 export const unlike = postId => dispatch =>
   unlikePost(postId)
-    .then( like =>
-      dispatch(removeLike(like)))
-    .catch( error =>
-      console.error(error));
+    .then(
+      like  => dispatch(removeLike(like)),
+      error => dispatch(handleLikeError(error))
+    )
+
+export const likeCb = (postId, isLiked ) => dispatch =>
+  dispatch( isLiked ? unlike(postId) : like(postId) );
