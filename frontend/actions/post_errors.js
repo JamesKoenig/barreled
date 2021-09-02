cf50ce1ec2ab2893504cfe884ed5cf48f6f63eac
James Koenig <Koenig.James@live.com>
2021-09-02 12:47:17 -0700

    fix: post error functions were undefined

--- frontend/actions/post_errors.js
+++ frontend/actions/post_errors.js
@@ -1,11 +1,11 @@
 export const RECEIVE_POST_ERRORS = "RECEIVE_POST_ERRORS";
 export const CLEAR_POST_ERRORS   = "CLEAR_POST_ERRORS";
 
-const receivePostErrors = errors => ({
+export const receivePostErrors = errors => ({
   type: RECEIVE_POST_ERRORS,
   errors,
 });
 
-const clearPostErrors = () => ({
+export const clearPostErrors = () => ({
   type: CLEAR_POST_ERRORS,
 });
