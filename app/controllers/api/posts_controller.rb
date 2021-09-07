ec5d914bcd268bc96bdb128474bc9e36dcc525da
James Koenig <Koenig.James@live.com>
2021-09-07 10:46:09 -0700

    send a 404 when the post hasn't been found

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,9 +1,13 @@
 class Api::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    like = Like.find_by(post_id: @post.id, user_id: current_user.id)
-    @post.post_liked = !!like
-    render :show
+    if !@post
+      render json: @post.errors.full_messages, status: 404
+    else
+      like = Like.find_by(post_id: @post.id, user_id: current_user.id)
+      @post.post_liked = !!like
+      render :show
+    end
   end
 
   def create
