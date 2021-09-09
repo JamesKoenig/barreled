fc6f48c84f2b4d984281657fb48dad5a23d38504
James Koenig <Koenig.James@live.com>
2021-09-09 13:56:29 -0700

    use case fall-through to add like updates to redux

--- frontend/reducers/entities/posts.js
+++ frontend/reducers/entities/posts.js
@@ -1,9 +1,15 @@
 import { RECEIVE_POSTS } from '../../actions/posts';
+import {
+  RECEIVE_LIKE,
+  REMOVE_LIKE,
+} from '../../actions/like_actions';
 
 export default (oldState = {}, action) => {
   Object.freeze(oldState);
   switch(action.type) {
     case RECEIVE_POSTS:
+    case RECEIVE_LIKE:
+    case REMOVE_LIKE:
       return Object.assign({},oldState,action.posts);
     default:
       return oldState;
