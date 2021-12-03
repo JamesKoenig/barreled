6b1ae8e6b0f3fee93bedc1a805332af37e4cf3b0
James Koenig <Koenig.James@live.com>
2021-12-02 18:16:15 -0800

    remove post-show-side divs use background-image
    this also moves to a % instead of a set 300px width

--- frontend/components/posts/show.jsx
+++ frontend/components/posts/show.jsx
@@ -4,10 +4,8 @@ import PostDisplay from './post_display_container';
 
 export default () => (
   <div id="post-show">
-    <div className="post-show-side"></div>
     <div className="post-show-centerize">
         <PostDisplay />
     </div>
-    <div className="post-show-side"></div>
   </div>
 )
