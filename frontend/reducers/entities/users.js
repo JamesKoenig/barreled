025bd75f612a3351fcaafa0c8499abac2098c7a8
James Koenig <Koenig.James@live.com>
2022-01-03 14:58:33 -0800

    add follow api handling to user reducer

--- frontend/reducers/entities/users.js
+++ frontend/reducers/entities/users.js
@@ -1,10 +1,21 @@
 import { RECEIVE_USERS } from '../../actions/users';
+import { RECEIVE_FOLLOW_STATUS } from '../../actions/follow_actions';
 
 export default (oldState = {}, action) => {
   Object.freeze(oldState);
   switch(action.type) {
     case RECEIVE_USERS:
       return Object.assign({},oldState,action.users);
+    case RECEIVE_FOLLOW_STATUS:
+      let res = Object.assign({}, oldState);
+
+      const key = Object.keys(action.followStatus).pop()
+      res[key] = {
+        ...res[key],
+        ...action.followStatus[key],
+      }
+
+      return res
     default:
       return oldState;
   }
