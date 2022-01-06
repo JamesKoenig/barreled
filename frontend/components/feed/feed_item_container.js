60cd31b99438c2bb15531811a15fbee099442875
James Koenig <Koenig.James@live.com>
2022-01-06 11:16:20 -0800

    remove unused redux container file

--- frontend/components/feed/feed_item_container.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import FeedItem from './feed_item';
-
-const mSTP = ({ entities: { posts, users } }, { feedItem }) => {
-  return {
-  };
-};
-
-export default connect(mSTP)(FeedItem);
