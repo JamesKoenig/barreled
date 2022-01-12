76c1a5382fe054132fb9c47477656c7a0d5169c1
James Koenig <Koenig.James@live.com>
2022-01-12 00:16:32 -0800

    linting modal, remove unused imports

--- frontend/components/modal/modal_container.jsx
+++ frontend/components/modal/modal_container.jsx
@@ -1,8 +1,8 @@
-import   Modal        from './modal';
-import { closeModal } from '../../actions/modal';
-import { connect    } from 'react-redux';
+import   Modal        from "./modal";
+import { closeModal } from "../../actions/modal";
+import { connect    } from "react-redux";
 
-import modalManifest from './modal_manifest';
+import modalManifest from "./modal_manifest";
 
 const mSTP = ({ui: { modal: { type, misc } } } ) => ({
   Component: modalManifest[type],
