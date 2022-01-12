518bbd7d800674ebb565fbe16ffcf4a227edd1e5
James Koenig <Koenig.James@live.com>
2022-01-11 18:45:32 -0800

    linting 404, setting up strings to be escaped

--- frontend/components/404/404.jsx
+++ frontend/components/404/404.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import Splash from '../splash/splash_container';
+import React from "react";
 
 const Four04 = () => (
   <div id="splash-pane">
     <div className="slide-flexer">
-        <div className="splash-box">
-          <h1 className="logo-char">
-            There's nothing here.
-          </h1>
-          <div className="auth-form-container">
-          <p>Whatever you were looking for doesn't currently exist at this
-             address, unless you were looking for this error page, in which
-             case: congrats! you totally found it.</p>
-          </div>
+      <div className="splash-box">
+        <h1 className="logo-char">
+          {"There's nothing here."}
+        </h1>
+        <div className="auth-form-container">
+          <p>
+            {
+              "Whatever you were looking for doesn't currently exist at this "+
+              "address, unless you were looking for this error page, in which"+
+              "case: congrats! you totally found it."
+            }
+          </p>
         </div>
+      </div>
     </div>
   </div>
-)
+);
 
 export default Four04;
