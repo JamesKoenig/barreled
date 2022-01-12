ae3368a876a74fc15f8e1803011d4535c76cfa7f
James Koenig <Koenig.James@live.com>
2022-01-11 19:01:03 -0800

    linting the store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,8 @@
-import { 
+import {
   createStore,
-  applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import RootReducer from '../reducers/root';
+  applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import RootReducer from "../reducers/root";
 
 export default (preloadedState = {}) =>
   createStore(RootReducer, preloadedState, applyMiddleware(thunk));
