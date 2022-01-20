96bccd145e50e1565f464f1ef3836f2bd87f8079
James Koenig <Koenig.James@live.com>
2022-01-20 03:55:33 -0800

    add fall-through starter switch

--- frontend/reducers/entities/featured_users.js
+++ frontend/reducers/entities/featured_users.js
@@ -2,8 +2,12 @@ import {
   RECEIVE_FEATURED_USERS,
 } from "../../actions/featured_users_actions";
 
-export default (oldState = {}) => {
+export default (oldState = {}, action) => {
   Object.freeze(oldState);
-  //switch statement would go here
-  return oldState;
-}
+  switch(action.type) {
+    case RECEIVE_FEATURED_USERS:
+      // break omitted: purposeful fall-through placeholder
+    default:
+      return oldState;
+  }
+};
