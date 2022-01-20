20c490d068589f612aaa274268614aace8041d6b
James Koenig <Koenig.James@live.com>
2022-01-20 05:30:10 -0800

    flesh featured component getting user information

--- frontend/components/featured/featured.jsx
+++ frontend/components/featured/featured.jsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, {
+  useEffect,
+} from "react";
 
-const FeaturedAccount = ({idx}) => (
+const FeaturedAccount = ({userName}) => (
   <li>
-    <p>{`account number ${idx}`}</p>
+    <p>{`${userName}`}</p>
   </li>
 );
 
-const Featured = () => (
-  <div className="featured-accounts">
-    <h3>featured accounts</h3>
-    <ul>
-      { [1,2,3,4,5,6].map( idx =>
-        (<FeaturedAccount key={`featured-element-${idx}`}
-                          idx={idx}/>) ) }
-    </ul>
-  </div>
-);
+const Featured = ({featuredUsers, getFeatured}) => {
+  useEffect( () => {
+    getFeatured();
+  }, []);
+
+  return (
+    <div className="featured-accounts">
+      <h3>featured accounts</h3>
+      <ul>
+        { featuredUsers.map( ({id,username}) => {
+          console.log(id);
+          console.log(username);
+          return (<FeaturedAccount key={`featured-user-${id}`} 
+                            userId={id}
+                            userName={username} />) }) }
+      </ul>
+    </div>
+  );
+};
 
 export default Featured;
