ef0fd02eb2c474cf2b0a27bca23e6cd5f0950cee
James Koenig <Koenig.James@live.com>
2022-01-20 04:42:40 -0800

    add featured users to store via reducer

--- frontend/reducers/entities/featured_users.js
+++ frontend/reducers/entities/featured_users.js
@@ -2,11 +2,11 @@ import {
   RECEIVE_FEATURED_USERS,
 } from "../../actions/featured_users_actions";
 
-export default (oldState = {}, action) => {
+export default (oldState = [], action) => {
   Object.freeze(oldState);
   switch(action.type) {
     case RECEIVE_FEATURED_USERS:
-      // break omitted: purposeful fall-through placeholder
+      return action.featuredUsers;
     default:
       return oldState;
   }
