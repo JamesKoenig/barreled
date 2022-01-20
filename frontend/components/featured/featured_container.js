9961fa3f7d25f55e3511bfa421c7608b588b556a
James Koenig <Koenig.James@live.com>
2022-01-20 04:56:35 -0800

    set up the state & dispatch to props connections

--- frontend/components/featured/featured_container.js
+++ frontend/components/featured/featured_container.js
@@ -1,4 +1,13 @@
 import { connect } from "react-redux";
 import Featured from "./featured";
+import { getFeatured } from "../../actions/featured_users_actions";
 
-export default connect()(Featured);
+const mSTP = state => ({
+  featuredUsers: state.entities.featuredUsers,
+});
+
+const mDTP = {
+  getFeatured
+};
+
+export default connect(mSTP, mDTP)(Featured);
