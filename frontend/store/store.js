c4c15558af8c5882004fe84fe06d480462f47009
James Koenig <Koenig.James@live.com>
2022-02-09 17:34:49 -0800

    use redux-logger when in development (4 debugging)

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,8 +1,28 @@
 import {
   createStore,
-  applyMiddleware } from "redux";
+  applyMiddleware
+} from "redux";
+
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import RootReducer from "../reducers/root";
 
 export default (preloadedState = {}) =>
-  createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(
+      /* add logger to middleware only if we're not in production, otherwise
+       *  just have thunk be the applied middleware.
+       *
+       * There might be a better way to do this than using ... spreading & array
+       *  concat since this is sorta involved-looking, but this should work
+       */
+      ...([thunk].concat(
+        process.env.NODE_ENV !== "production"
+          ? logger
+          : []
+        ) // the hell of closing )'s... indentation saves lives.
+      )
+    )
+  );
